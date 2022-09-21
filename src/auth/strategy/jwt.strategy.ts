3c437fc1b69d8a1d3a18381dc44517b889613c07
shabni <ahsanahmad@gmail.com>
2022-09-21 11:54:42 +0500

    get user after Authorisation

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,22 +2,28 @@ import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { PrismaService } from "src/prisma/prisma.service";
 
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 
-    constructor(config: ConfigService) {
+    constructor(config: ConfigService, private prisma: PrismaService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: config.get('JWT_SECRET')
         })
     }
 
-    validate(payload: any) {
-        console.log({payload},"--->>>")
+    async validate(payload: any) {
 
-        return payload
+        const user = await this.prisma.user.findFirst({
+            where: {
+                id: payload.sub
+            }
+        })
+
+        return user
     }
 
 }
